ce643f598dd2e3291813161da49f93fa9e082951
godcong <jumbycc@163.com>
2019-08-09 13:45:38 +0800

    update main

--- go.mod
+++ go.mod
@@ -12,25 +12,22 @@ require (
 	github.com/ethereum/go-ethereum v1.8.27
 	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
 	github.com/glvd/bot v0.0.0-20190806104516-48f1f01c0e2a
-	github.com/glvd/seed v0.0.0-20190806100046-c4181ed6e320
+	github.com/glvd/seed v0.0.0-20190809054306-ac1aaf67d476
 	github.com/go-xorm/xorm v0.7.1
 	github.com/gocarina/gocsv v0.0.0-20190624090726-71ad6676df57
 	github.com/godcong/go-ipfs-restapi v0.0.3-0.20190422054229-f13eeaa39df0
 	github.com/godcong/go-trait v0.0.0-20190528080809-9a857488365f
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/huin/goupnp v1.0.0 // indirect
-	github.com/jackpal/go-nat-pmp v1.0.1 // indirect
 	github.com/json-iterator/go v1.1.6
 	github.com/karalabe/hid v1.0.0 // indirect
 	github.com/kr/pty v1.1.3 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/rjeczalik/notify v0.9.2 // indirect
-	github.com/rs/cors v1.6.0 // indirect
-	github.com/syndtr/goleveldb v1.0.0 // indirect
-	golang.org/x/xerrors v0.0.0-20190510150013-5403a72a6aaf
+	golang.org/x/xerrors v0.0.0-20190513163551-3ee3066db522
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/urfave/cli.v2 v2.0.0-20180128182452-d3ae77c26ac8
 )
+
+replace github.com/ipfs/go-ipfs-http-client => github.com/godcong/go-ipfs-http-client v0.0.4-0.20190808064056-6732621f7cd6
--- pin/pin_run.go
+++ pin/pin_run.go
@@ -16,16 +16,11 @@ func CmdPin(app *cli.App) *cli.Command {
 			Name:  "status",
 			Usage: "set the pin status (all/relate/hash/unfinished) default is all",
 		},
-		//&cli.StringFlag{
-		//	Name:    "release",
-		//	Value:   "v0.0.1",
-		//	Aliases: []string{"r"},
-		//	Usage:   "set the application version",
-		//},
-		//&cli.StringFlag{
-		//	Name:  "hash",
-		//	Usage: "set the app ipfs hash",
-		//},
+		&cli.StringFlag{
+			Name:        "type",
+			DefaultText: "all",
+			Usage:       "set the pin check type",
+		},
 		//&cli.StringFlag{
 		//	Name:  "path",
 		//	Usage: "set the app path to add  hash",
@@ -48,55 +43,127 @@ func CmdPin(app *cli.App) *cli.Command {
 		Name:    "pin",
 		Aliases: []string{"P"},
 		Usage:   "pin file to ipfs",
-		Action: func(context *cli.Context) error {
-			db := context.String("database")
-			if db == "" {
-				db = "cs.db"
-			}
-			eng, e := model.InitDB("sqlite3", db)
-			if e != nil {
-				return e
-			}
-			model.InitMainDB(eng)
-			ps := seed.PinStatusAll
-			switch context.String("status") {
-			case "relate":
-				ps = seed.PinStatusAssignRelate
-			case "hash":
-				ps = seed.PinStatusAssignHash
-			case "unfinished":
-				ps = seed.PinStatusUnfinished
-			case "slice":
-				ps = seed.PinStatusSliceOnly
-			case "video":
-				ps = seed.PinStatusVideo
-			case "poster":
-				ps = seed.PinStatusPoster
-			}
+		Action:  nil,
+		Subcommands: []*cli.Command{
+			{
+				Name:          "add",
+				Aliases:       nil,
+				Usage:         "",
+				UsageText:     "",
+				Description:   "",
+				ArgsUsage:     "",
+				Category:      "",
+				ShellComplete: nil,
+				Before:        nil,
+				After:         nil,
+				Action: func(context *cli.Context) error {
+					db := context.String("database")
+					if db == "" {
+						db = "cs.db"
+					}
+					eng, e := model.InitDB("sqlite3", db)
+					if e != nil {
+						return e
+					}
+					model.InitMainDB(eng)
+					ps := seed.PinStatusAll
+					switch context.String("status") {
+					case "relate":
+						ps = seed.PinStatusAssignRelate
+					case "hash":
+						ps = seed.PinStatusAssignHash
+					case "unfinished":
+						ps = seed.PinStatusUnfinished
+					case "slice":
+						ps = seed.PinStatusSliceOnly
+					case "video":
+						ps = seed.PinStatusVideo
+					case "poster":
+						ps = seed.PinStatusPoster
+					}
 
-			s := seed.NewSeed(seed.DatabaseOption("sqlite3", db), seed.Pin(ps, context.Args().Slice()...))
-			j := context.String("json")
-			if j != "" {
-				s.Register(seed.Information(context.String("json"), seed.InfoFlagBSON))
-			}
-			shell := context.String("shell")
-			if shell != "" {
-				s.Register(seed.ShellOption(shell))
-			}
+					s := seed.NewSeed(seed.DatabaseOption("sqlite3", db), seed.Pin(ps, context.Args().Slice()...))
+					j := context.String("json")
+					if j != "" {
+						s.Register(seed.Information(context.String("json"), seed.InfoFlagBSON))
+					}
+					shell := context.String("shell")
+					if shell != "" {
+						s.Register(seed.ShellOption(shell))
+					}
 
-			if context.Bool("skip") {
-				s.Register(seed.SkipConvertOption())
-				s.Register(seed.SkipSourceOption())
-			}
+					if context.Bool("skip") {
+						s.Register(seed.SkipConvertOption())
+						s.Register(seed.SkipSourceOption())
+					}
 
-			s.AfterInit(seed.SyncDatabase())
-			s.Workspace = context.String("workspace")
-			s.Start()
+					s.AfterInit(seed.SyncDatabase())
+					s.Workspace = context.String("workspace")
+					s.Start()
 
-			s.Wait()
-			return nil
+					s.Wait()
+					return nil
+				},
+				OnUsageError:       nil,
+				Subcommands:        nil,
+				Flags:              flags,
+				SkipFlagParsing:    false,
+				HideHelp:           false,
+				Hidden:             false,
+				HelpName:           "",
+				CustomHelpTemplate: "",
+			},
+			{
+				Name:          "check",
+				Aliases:       nil,
+				Usage:         "",
+				UsageText:     "",
+				Description:   "",
+				ArgsUsage:     "",
+				Category:      "",
+				ShellComplete: nil,
+				Before:        nil,
+				After:         nil,
+				Action: func(context *cli.Context) error {
+					db := context.String("database")
+					if db == "" {
+						db = "cs.db"
+					}
+					eng, e := model.InitDB("sqlite3", db)
+					if e != nil {
+						return e
+					}
+					model.InitMainDB(eng)
+					tp := context.String("type")
+
+					s := seed.NewSeed(seed.DatabaseOption("sqlite3", db), seed.Check(tp))
+
+					api := context.String("api")
+					if api != "" {
+						s.Register(seed.APIOption(api))
+					}
+
+					shell := context.String("shell")
+					if shell != "" {
+						s.Register(seed.ShellOption(shell))
+					}
+
+					s.AfterInit(seed.SyncDatabase())
+					s.Workspace = context.String("workspace")
+					s.Start()
+
+					s.Wait()
+					return nil
+				},
+				OnUsageError:       nil,
+				Subcommands:        nil,
+				Flags:              flags,
+				SkipFlagParsing:    false,
+				HideHelp:           false,
+				Hidden:             false,
+				HelpName:           "",
+				CustomHelpTemplate: "",
+			},
 		},
-		Subcommands: nil,
-		Flags:       flags,
 	}
 }
