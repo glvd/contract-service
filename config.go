ac013f9a62ae200cf66412866f40994ef79015b0
godcong <jumbycc@163.com>
2019-10-24 18:19:36 +0800

    update contract

--- config.go
+++ config.go
@@ -13,3 +13,16 @@ type Config struct {
 	ConversionLimit int            `json:"conversion_limit"`
 	Database        DatabaseConfig `json:"database"`
 }
+
+func DefaultConfig() *Config {
+	return &Config{
+		RestPort:        "8084",
+		RPCPort:         "8095",
+		ConversionLimit: 1,
+		Database: DatabaseConfig{
+			Addr:     "localhost:3306",
+			Username: "root",
+			Password: "111111",
+		},
+	}
+}
