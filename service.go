69536ca94ece9539607c57417629bb8e23337910
godcong <jumbycc@163.com>
2019-10-24 17:33:11 +0800

    update contract

--- service.go
+++ service.go
@@ -2,21 +2,41 @@ package service
 
 import (
 	"github.com/glvd/conversion"
+	"github.com/goextension/log"
 	"github.com/goextension/tool"
 )
 
 // Service ...
 type Service struct {
+	config Config
 	secret string
 	task   *conversion.Task
 }
 
 // NewService ...
 func NewService() *Service {
+
 	secret := tool.GenerateRandomString(32)
 	task := initConversion()
 	return &Service{
 		secret: secret,
 		task:   task,
 	}
+}
+
+// Start ...
+func (s *Service) Start() error {
+	//start conversion task
+	go func() {
+		if err := s.task.Start(); err != nil {
+			log.Panicw(err.Error(), "func", "service|start")
+			return
+		}
+	}()
+	return nil
+}
+
+// Stop ...
+func (s *Service) Stop() {
+	s.task.Stop()
 }
