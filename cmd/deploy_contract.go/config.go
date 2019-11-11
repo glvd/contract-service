16010df7150942cf4260241ee399b859e7f3baf9
godcong <jumbycc@163.com>
2019-11-11 17:25:09 +0800

    update service

---
+++ cmd/deploy_contract.go/config.go
@@ -0,0 +1,16 @@
+package main
+
+type config struct {
+	Database string
+}
+
+// DefaultConfig ...
+var DefaultConfig = "deploy.json"
+
+// DefaultConfigPath ...
+var DefaultConfigPath = "config"
+
+// LoadConfig ...
+func LoadConfig() {
+
+}
