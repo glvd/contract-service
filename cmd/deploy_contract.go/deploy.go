2842754288ee9b99c6fe69551981d4695ea31fc7
godcong <jumbycc@163.com>
2019-11-11 17:28:02 +0800

    update service

--- cmd/deploy_contract.go/deploy.go
+++ cmd/deploy_contract.go/deploy.go
@@ -10,7 +10,21 @@ func main() {
 	app := cli.NewApp()
 	app.Version = "v0.0.1"
 	app.Name = applicationName
-
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "config, c",
+			Aliases:     nil,
+			Usage:       "",
+			EnvVars:     nil,
+			FilePath:    "",
+			Required:    false,
+			Hidden:      false,
+			TakesFile:   false,
+			Value:       "",
+			DefaultText: "",
+			Destination: nil,
+		},
+	}
 	//Usage:   "service is a video manage tool use ipfs,eth,sqlite3 and so on.",
 	//Action: func(c *cli.Context) error {
 	//return nil
