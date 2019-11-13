674c56f49759b839955db2d006b33a053b8a489f
godcong <jumbycc@163.com>
2019-11-13 17:58:33 +0800

    update service

---
+++ cmd/contract/deployrun.bat
@@ -0,0 +1,3 @@
+cd %CD%
+
+contract.exe --message 0xdfbff0f931cf056b9eb5a8b58616024881215f01 --tag 0x431d6215052cd3f0cec1838289a3d99abc496f5e --keypath bd53e69c2afe7d549ba1855c2b1dad307cf0512a --keypass 123 --gateway http://139.196.215.224:8545
