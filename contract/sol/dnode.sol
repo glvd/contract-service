a92a9f39ea69cde938cb57ccb8d3e59335bb7c89
godcong <jumbycc@163.com>
2019-11-13 15:31:45 +0800

    update service

--- contract/sol/dnode.sol
+++ contract/sol/dnode.sol
@@ -10,53 +10,60 @@ contract DNode is Ownable{
     }
 
     struct Version{
+        uint   cid;//client id
         string hash;
         string version;
     }
+
+    mapping(uint => uint) mappingDevices;
 
     mapping(uint => Node)  mappingNodes;
     uint private last;
 
     mapping(uint => Version) mappingVersions;
-    uint private lastVersion;
+    uint private totalVersion;
 
-    function setLast(uint idx) public onlyOwner{
-        require(idx > last, "Node: last node is newer than now");
-        last = idx;
+    function setNodeLast(uint _idx) public onlyOwner{
+        require(_idx > last, "Node: last node is newer than now");
+        last = _idx;
     }
 
-    function getLast() public view returns(uint) {
+    function getNodeLast() public view returns(uint) {
         return last;
     }
 
-    function getLastVersion() public view returns(string memory,string memory){
-        require(lastVersion > 0, "Version: index version is not exist");
-        return getVersion(lastVersion-1);
+    function getDeviceVersion(uint _cid) public view returns(Version memory _version){
+        require(mappingDevices[_cid] > 0, "Version: index version is not exist");
+        return getVersion(mappingDevices[_cid]);
     }
 
-    function getVersion(uint idx)public view returns(string memory,string memory){
-        return (mappingVersions[idx].version,mappingVersions[idx].hash);
+    function setDeviceVersion(uint _cid,string memory _ver,string memory _hash)public onlyOwner{
+        mappingDevices[_cid]= totalVersion;
+        setVersion(_cid,_ver,_hash);
     }
 
-    function setVersion(uint idx,string memory ver,string memory hash)public onlyOwner{
-        require(idx < lastVersion, "Version: index version is not exist");
-        mappingVersions[idx] = Version({version:ver,hash:hash});
+    function getVersion(uint _idx)public view returns(Version memory _version){
+        return mappingVersions[_idx];
     }
 
-    function setLastVersion(string memory ver,string memory hash)public onlyOwner{
-        mappingVersions[lastVersion] = Version({version:ver,hash:hash});
-        lastVersion++;
+    function setVersion(uint _cid,string memory _ver,string memory _hash)private onlyOwner{
+        mappingVersions[totalVersion] = Version({cid:_cid,version:_ver,hash:_hash});
+        totalVersion++;
     }
 
-    function remove(uint idx) public onlyOwner{
-        require(idx < mappingNodes[last].count, "Node: delete node is not exist");
+    function replaceVersion(uint _idx,uint _cid,string memory _ver,string memory _hash)public onlyOwner{
+        mappingVersions[_idx] = Version({cid:_cid,version:_ver,hash:_hash});
+    }
+
+    function removeNode(uint _idx) public onlyOwner{
+        require(_idx < mappingNodes[last].count, "Node: delete node is not exist");
         uint limit = mappingNodes[last].count;
-        mappingNodes[last].swarms[idx] = mappingNodes[last].swarms[limit-1];
+        mappingNodes[last].swarms[_idx] = mappingNodes[last].swarms[limit-1];
         mappingNodes[last].count--;
     }
 
-    function store(string memory swarm)public onlyOwner{
-         mappingNodes[last].swarms[mappingNodes[last].count] = swarm;
+    function storeNode(string memory _swarm)public onlyOwner{
+         mappingNodes[last].swarms[mappingNodes[last].count] = _swarm;
          mappingNodes[last].count++;
     }
 
@@ -64,11 +71,11 @@ contract DNode is Ownable{
         return getNode(last);
     }
 
-    function getNode(uint idx) public view returns(string[] memory,uint){
-        uint limit = mappingNodes[idx].count;
+    function getNode(uint _idx) public view returns(string[] memory,uint){
+        uint limit = mappingNodes[_idx].count;
         string[] memory swarms = new string[](limit);
         for (uint i = 0 ; i < limit ; i++){
-            swarms[i] = mappingNodes[idx].swarms[i];
+            swarms[i] = mappingNodes[_idx].swarms[i];
         }
         return (swarms,limit);
     }
