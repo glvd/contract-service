400ab4c2021f715795aae00121a144767ab53b02
godcong <jumbycc@163.com>
2019-11-27 16:10:04 +0800

    update service

--- cmd/tools/deploy.go
+++ cmd/tools/deploy.go
@@ -154,7 +154,7 @@ func deployBefore() cli.BeforeFunc {
 
 		_contract = contract.NewContract(
 			contract.ETHClient(contract.DefaultGatway),
-			contract.FileKey(_config.KeyPath, _config.KeyPass),
+			contract.FileKey(DefaultKeyPath, DefaultKeyPass),
 			//contract.HexKey("9efef8ebc3c51e91fb7f9faf7dbd516cb320ade03108c1568c9cee01a39af311"),
 			contract.Node(contract.DefaultNodeAddress),
 			contract.Tag(contract.DefaultTagAddress),
